e7b66e46b8230152e016569c6d176d867f4fa7c0
pam <phloi.p@gmail.com>
2016-02-20 14:02:17 +0700

    Explicit เสร็จแล้ว

--- app/src/main/java/chairattanangkul/ployrung/mytraffic/MainActivity.java
+++ app/src/main/java/chairattanangkul/ployrung/mytraffic/MainActivity.java
@@ -2,12 +2,21 @@ package chairattanangkul.ployrung.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    //ประกาศตัวแปรในคลาสเหนือ method
+    //Explicit บอก device ว่า ram พอป่าว
+    //ship + ctrl + enter มันจะเติม สัญลักษณ์ข้างหลังให้ , ctrl +atl +L จะเรียงความสวย ctrl + space จะ suggest ให้
+
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }   //Main Method
+}   //Main Class
