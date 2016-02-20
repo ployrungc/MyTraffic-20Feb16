ddf0751dcb4bf94cd6564fc256e88b4e73e362bb
pam <phloi.p@gmail.com>
2016-02-20 14:29:46 +0700

    Bind Widget done

--- app/src/main/java/chairattanangkul/ployrung/mytraffic/MainActivity.java
+++ app/src/main/java/chairattanangkul/ployrung/mytraffic/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.ListView;
 public class MainActivity extends AppCompatActivity {
     //ประกาศตัวแปรในคลาสเหนือ method
     //Explicit บอก device ว่า ram พอป่าว
-    //ship + ctrl + enter มันจะเติม สัญลักษณ์ข้างหลังให้ , ctrl +atl +L จะเรียงความสวย ctrl + space จะ suggest ให้
+    //ship + ctrl + enter มันจะเติม สัญลักษณ์ข้างหลังให้ , ctrl +atl +L จะเรียงความสวย ctrl + space จะ suggest ให้ alt+enter
 
     private ListView trafficListView;
     private Button aboutMeButton;
@@ -18,5 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Bind widget
+        bindWidget();
+
+
     }   //Main Method
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+    }
 }   //Main Class
